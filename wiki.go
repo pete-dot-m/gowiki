8113d78e2659080bd7503dd12646b3c6ac6f349c
Pete McQuain <434857+pete-dot-m@users.noreply.github.com>
2024-02-01 08:30:05 -0500

    Add files via upload
    Moved index handler to template. Added foundation.css. Added logging via middleware

--- wiki.go
+++ wiki.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"errors"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"regexp"
 	"strings"
+	"time"
 )
 
 type Page struct {
@@ -19,6 +21,39 @@ var (
 	validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 )
 
+// Helper to load page files from the data directory, creating it if it doesn't exist
+func getDataFileNames(path string) ([]string, error) {
+	var fileNames []string
+
+	// check that the directory exists, create it if not...
+	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+		err := os.Mkdir(path, os.ModePerm)
+		if err != nil {
+			log.Printf("Directory %s doesn't exist and couldn't create\n", path)
+			return fileNames, err
+		}
+	}
+
+	// open the directory and get the files
+	f, err := os.Open(path)
+	if err != nil {
+		log.Printf("Couldn't open directory %s: %s\n", path, err.Error())
+		return fileNames, err
+	}
+	files, err := f.Readdir(-1)
+	if err != nil {
+		log.Printf("Couldn't read directory %s: %s\n", path, err.Error())
+		return fileNames, err
+	}
+
+	for _, file := range files {
+		name, _ := strings.CutSuffix(file.Name(), ".txt")
+		fileNames = append(fileNames, name)
+	}
+	return fileNames, nil
+}
+
+// Page load and save functions
 func (p *Page) save() error {
 	filename := "data/" + p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
@@ -33,12 +68,14 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+// Template helpers
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	if err := templates.ExecuteTemplate(w, tmpl+".html", p); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
+// The HttpHandler funcs
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
@@ -68,25 +105,32 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	files, err := os.ReadDir("data")
+	files, err := getDataFileNames("data")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	names := make([]string, len(files))
-	for _, n := range files {
-		log.Println(n.Name())
-		name, _ := strings.CutSuffix(n.Name(), ".txt")
-		names = append(names, name)
-	}
-	log.Printf("len(names)=%d\n", len(names))
-	if err = templates.ExecuteTemplate(w, "index.html", names); err != nil {
+	if err = templates.ExecuteTemplate(w, "index.html", files); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
 	}
 }
 
+// logging middleware
+func logRequestHandler(h http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		// call the original handler we're wrapping
+		h.ServeHTTP(w, r)
+
+		// gather information about the request and log it
+		uri := r.URL.String()
+		method := r.Method
+
+		log.Printf("%s:%s", uri, method)
+	}
+	return http.HandlerFunc(fn)
+}
+
+// HttpHandler wrapper to ensure valid paths are being passed into our handlers
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		m := validPath.FindStringSubmatch(r.URL.Path)
@@ -98,10 +142,23 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 	}
 }
 
+// Where all the magic happens...
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/view/", makeHandler(viewHandler))
-	http.HandleFunc("/edit/", makeHandler(editHandler))
-	http.HandleFunc("/save/", makeHandler(saveHandler))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	mux := &http.ServeMux{}
+
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/view/", makeHandler(viewHandler))
+	mux.HandleFunc("/edit/", makeHandler(editHandler))
+	mux.HandleFunc("/save/", makeHandler(saveHandler))
+
+	var handler http.Handler = mux
+	handler = logRequestHandler(handler)
+	srv := &http.Server{
+		ReadTimeout:  120 * time.Second,
+		WriteTimeout: 120 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		Handler:      handler,
+		Addr:         ":8080",
+	}
+	log.Fatal(srv.ListenAndServe())
 }
